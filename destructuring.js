31c23680d1690c3081e6d79bcf989690a30e682e
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-06-22 09:07:38 +0300

    destructuring arrays html and js file day 11

---
+++ destructuring.js
@@ -0,0 +1,10 @@
+// destructuring and spread 
+// destructuring is a way to unpack arrays and objects and assigning to a distinct variable 
+const numbers = [1,2,3,4,5]
+let [numOne, numTwo, numThree, numFour,numFive] = numbers
+console.log(numOne, numTwo, numThree, numFour, numFive)
+
+// example two 
+const names = ['Asabeneh', 'Brook', 'David', 'John']
+let [firstName,secondName,thirdName, fourthName]= names
+console.log(firstName,secondName,thirdName, fourthName)
